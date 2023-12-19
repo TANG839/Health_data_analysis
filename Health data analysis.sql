eece44fbc2b6c179c488c91ffda3ea792b2df55d
Fuyao Tang <144066008+TANG839@users.noreply.github.com>
2023-12-20 00:33:52 +0100

    Update Health data analysis.sql

--- Health data analysis.sql
+++ Health data analysis.sql
@@ -25,9 +25,38 @@ Order by 2;
 -- alter Table health_data.ocd_patient_dataset
 -- modify `OCD Diagnosis Date` date;               change this column to date format
 select
-date_format(`OCD Diagnosis Date`,`%Y-%m-01 00:00"00`) as month,
+date_format(`OCD Diagnosis Date`, '%Y-%m-01 00:00:00') as month,
+count(`Patient ID`) patient_count
+FROM health_data.ocd_patient_dataset
+group by 1
+Order by 1;
 
-from 
+
+
+-- 4. What is the most common Obsession Type (Count) & it's respective Average Obsession Score
+
+Select
+`Obsession Type`,
+count(`Patient ID`) as patient_count,
+round(avg(`Y-BOCS Score (Obsessions)`),2) as obs_score
+from health_data.ocd_patient_dataset
+group by 1
+Order by 2
+;
+
+
+
+-- 5. What is the most common Compulsion type (Count) & it's respective Average Obsession Score
+
+Select
+`Compulsion Type`,
+count(`Patient ID`) as patient_count,
+round(avg(`Y-BOCS Score (Obsessions)`),2) as obs_score
+from health_data.ocd_patient_dataset
+group by 1
+Order by 2
+;
+
 
 
 
